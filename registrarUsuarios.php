773fb6e392b599ae957d519bc72ef743f52a3290
Juan <juan.gallegomesa@gmail.com>
2020-11-13 08:20:30 -0500

    SE MODIFICA REGISTRO PARA AGREGAR FOTO

--- registrarUsuarios.php
+++ registrarUsuarios.php
@@ -9,13 +9,14 @@
         $apellido=$_POST["apellido"];
         $descripcion=$_POST["descripcion"];
         $genero=$_POST["genero"];
+        $foto=$_POST["foto"];
 
         //2. crear una copia de la clase BD
         //crear un objeto de la clase bd
         $transaccion=new Basedatos();
 
         //3. crear una consulta SQL para agregar datos
-        $consultaSQL="INSERT INTO usuarios(nombre, apellido, descripcion, genero) VALUES ('$nombre','$apellido','$descripcion','$genero')";
+        $consultaSQL="INSERT INTO usuarios(nombre, apellido, descripcion, genero, foto) VALUES ('$nombre','$apellido','$descripcion','$genero','$foto')";
 
         //4. Utilizar el metodo agregarDatos()
         $transaccion->agregarDatos($consultaSQL);
